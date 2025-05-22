0a1d3dc2379b9d94b67a4af5ae3ab13babf12595
SantyRuscio <santiruscio1237@gmail.com>
2025-05-22 01:12:03 -0300

    Nueva Scena
    Nueva Scena casa1

---
+++ Assets/Proyecto/Casa1/Scrips/AudioPizarron.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioPizarron : MonoBehaviour
+{
+    [SerializeField] private Image _miraEscondida;
+    [SerializeField] private TextMeshProUGUI _ComandanteRuscio;
+
+    [SerializeField] public AudioSource _Pizarron;
+    [SerializeField] private AudioClip sonidoPizarron;
+    private void Start()
+    {
+        _miraEscondida.gameObject.SetActive(false);
+        _ComandanteRuscio.gameObject.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        _Pizarron.PlayOneShot(sonidoPizarron);
+        _ComandanteRuscio.gameObject.SetActive(true);
+
+        GetComponent<BoxCollider>().enabled = false;
+
+        StartCoroutine(AutoDestruccionTime());
+    }
+
+    IEnumerator AutoDestruccionTime()
+    {
+        yield return new WaitForSeconds(6.2f);
+        _ComandanteRuscio.gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
+}
+
---
+++ Assets/Proyecto/Casa1/Scrips/Cama.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Cama : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _tocaLaLetra;
+
+    private void Start()
+    {
+        _tocaLaLetra.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("choque con algo");
+        _tocaLaLetra.gameObject.SetActive(true);
+
+        if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("CAMBIANDO DE ESCENA");
+            NextScene();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        _tocaLaLetra.gameObject.SetActive(false);
+    }
+    private void NextScene()
+    {
+        Debug.Log("CARGANDO ESCENA");
+       // SceneManager.LoadScene("Juego"); fALTA poner el nombre que va realmente
+    }
+}
+
