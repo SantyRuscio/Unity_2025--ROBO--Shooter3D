fae069fe251cb85985db9b682101e43155c19600
SantyRuscio <santiruscio1237@gmail.com>
2025-05-29 03:17:00 -0300

    Audiso

--- Assets/Proyecto/Casa1/Scrips/AudioGenerico.cs
+++ Assets/Proyecto/Casa1/Scrips/AudioGenerico.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AudioPizarron : MonoBehaviour
+public class AudioGenerico : MonoBehaviour
 {
     [SerializeField] private Image _miraEscondida;
     [SerializeField] private TextMeshProUGUI _Comandantes;
