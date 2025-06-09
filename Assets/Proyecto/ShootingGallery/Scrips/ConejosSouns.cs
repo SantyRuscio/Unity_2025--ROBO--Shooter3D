02bc0c7218f6ab46e7a1551a65337c75d3be193f
SantyRuscio <santiruscio1237@gmail.com>
2025-06-09 16:29:53 -0300

    Arreglos

--- Assets/Proyecto/ShootingGallery/Scrips/ConejosSouns.cs
+++ Assets/Proyecto/ShootingGallery/Scrips/ConejosSouns.cs
@@ -8,22 +8,31 @@ public class ConejosSouns : MonoBehaviour
 {
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private AudioClip _audioDianas;
+
+    [SerializeField] private AudioSource _audioSourceComandante;
     [SerializeField] private AudioClip _audioComandante;
+
+    private bool _audioComandanteReproducido = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Bullet"))
         {
             _audioSource.PlayOneShot(_audioDianas);
-            StartCoroutine(Comandante());
             Debug.Log("Me pegó una bala");
+
+            if (!_audioComandanteReproducido)
+            {
+                _audioComandanteReproducido = true;
+                StartCoroutine(Comandante());
+            }
         }
     }
 
     IEnumerator Comandante()
     {
         yield return new WaitForSeconds(6f);
-        _audioSource.PlayOneShot(_audioComandante);
+        _audioSourceComandante.PlayOneShot(_audioComandante);
         Debug.Log("Corrutina terminada");
     }
 }
