b636faee194264b6f7d5d1884efcb6249cd26006
Lázaro Berola <lazarobe89@gmail.com>
2025-07-02 03:53:50 -0300

    spawner en proceso
    hay que revisar porque no anda bien, se agregaron dos scripts, se agrego lineas de codigo bien señaladas en el script del diamante, y se agregaron 3 game objects vacios que funcionan como los spawners

---
+++ Assets/Scripts/Enemigos/EnemySpawner.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("Configuración de Spawn")]
+    public GameObject enemyPrefab;
+    public int maxEnemiesToSpawn = 10;
+    public float minSpawnTime = 2f;
+    public float maxSpawnTime = 3f;
+
+    [HideInInspector] public int enemiesSpawned = 0;
+    [HideInInspector] public int enemiesKilled = 0;
+
+    private bool isSpawning = false;
+
+    public void StartSpawning()
+    {
+        if (!isSpawning)
+        {
+            isSpawning = true;
+            StartCoroutine(SpawnEnemies());
+        }
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        while (enemiesSpawned < maxEnemiesToSpawn)
+        {
+            float waitTime = Random.Range(minSpawnTime, maxSpawnTime);
+            yield return new WaitForSeconds(waitTime);
+
+            GameObject newEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            EnemigoSpawneado enemyScript = newEnemy.AddComponent<EnemigoSpawneado>();
+            enemyScript.SetSpawner(this);
+
+            enemiesSpawned++;
+        }
+    }
+
+    public void NotifyEnemyDeath()
+    {
+        enemiesKilled++;
+        Debug.Log("Enemigo muerto. Total: " + enemiesKilled);
+    }
+}
